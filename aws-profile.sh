00c378b52a30774fad05c3fe745bccfde878793a
marvelph <marvel@programmershigh.org>
2026-03-11 14:08:52 +0900

    Update shell scripts

--- aws-profile.sh
+++ aws-profile.sh
@@ -1,6 +1,6 @@
 aws-profile() {
   local profile
-  profile=$(aws configure list-profiles | fzf --preview 'awk '\''( "{}" == "default" && $1 == "[default]" ) || ( $1 == "[profile" && $2 == "{}]" ) { found = 1 ; print $0 ; next } found && $1 ~ /^\[.+$/ { exit } found { print $0 }'\'' ~/.aws/config' --header="Select an AWS profile" --footer="aws-profile") || return 1
+  profile=$(aws configure list-profiles | fzf --preview 'awk '\''( "{}" == "default" && $1 == "[default]" ) || ( $1 == "[profile" && $2 == "{}]" ) { found = 1 ; print $0 ; next } found && $1 ~ /^\[.+$/ { exit } found { print $0 }'\'' ~/.aws/config' --header="Select an AWS profile" --footer="export AWS_PROFILE=<selected>") || return 1
   export AWS_PROFILE="$profile"
 }
 
